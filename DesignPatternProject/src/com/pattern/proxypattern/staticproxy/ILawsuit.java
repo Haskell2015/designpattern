a8eacd5ceebb362c2bc97d1aebf0732f4c5b684b
rogue1990@163.com <1990chenxiao>
2017-07-26 21:10:21 +0800

    add proxy pattern

---
+++ DesignPatternProject/src/com/pattern/proxypattern/staticproxy/ILawsuit.java
@@ -0,0 +1,10 @@
+package com.pattern.proxypattern.staticproxy;
+
+public interface ILawsuit {
+    //提交申请
+    void submit();
+    //v进行举证
+    void burden();
+    void defend();
+    void finish();
+}
